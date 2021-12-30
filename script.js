af39e02966fe29950ce27f7790095ebef19402a1
rodged <96698457+rodged@users.noreply.github.com>
2021-12-30 03:11:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+        const Locoscroll = new LocomotiveScroll({
+            el:document.querySelector([".worefa"]),
+            smooth:true,
+            smartphone:{
+                smooth:true
+            }
+            
+        });
